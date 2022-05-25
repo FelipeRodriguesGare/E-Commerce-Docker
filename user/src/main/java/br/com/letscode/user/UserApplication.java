4f1d36950e19654db2a79f70cbfbde2f57609d9c
FelipeRodriguesGare <felipe.gare@hotmail.com>
2022-05-24 22:50:36 -0300

    adding prometheus and grafana

--- user/src/main/java/br/com/letscode/user/UserApplication.java
+++ user/src/main/java/br/com/letscode/user/UserApplication.java
@@ -3,6 +3,7 @@ package br.com.letscode.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class UserApplication {
